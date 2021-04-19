dca8452d600344beb68816fd6a7137f75b0a6ecc
Alexander Karpov <73037661+ImYourBuddy@users.noreply.github.com>
2021-04-19 11:02:26 +0300

    Add files via upload

---
+++ pet-web-app-backend/src/main/java/com/imyourbuddy/petwebapp/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.imyourbuddy.petwebapp.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
